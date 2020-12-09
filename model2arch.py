edd392d78a616d7285416c7fad5025fe0948b15d
styinx <styinx@users.noreply.github.com>
2020-12-09 11:56:54 +0000

    Deploying to gh-pages from  @ a2697abefbc4415a914b8c33a1242c160f7778cc 🚀

--- model2arch.py
+++ model2arch.py
@@ -1,10 +1,11 @@
+import pickle
 import argparse
 
 from models.architecture import Architecture
 from models.jaeger_trace import JaegerTrace
 from models.misim_model import MiSimModel
 from models.zipkin_trace import ZipkinTrace
-
+from util.parse import bool_from_string
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Converts a model into an architecture representation.')
@@ -21,26 +22,33 @@ if __name__ == '__main__':
 
     # Validation
     parser.add_argument('-vm', '--validate-model', dest='validate_model', action='store_true',
-                        help="Validates the model.")
+                        help='Validates the model.')
     parser.add_argument('-va', '--validate-architecture', dest='validate_architecture', action='store_true',
-                        help="Validates the architecture.")
+                        help='Validates the architecture.')
 
-    # Other
+    # Export
+    parser.add_argument('-em', '--export-model', dest='export', action='store_true',
+                        help='Exports the converted model in an intermediate format (pickle).')
     parser.add_argument('-d3', '--d3-graph', dest='d3', type=str, nargs='+', required=False,
-                        help="Stores a d3 graph to the specified location.")
+                        help='Stores a d3 graph to the specified location.')
 
     args = parser.parse_args()
     model = None
     arch = None
+    model_name = ''
 
     if args.model:
-        model = model
+        model_name = args.model[0]
+        model = pickle.load(open(model_name, 'rb'))
     elif args.misim:
-        model = MiSimModel(args.misim[0])
+        model_name = args.misim[0]
+        model = MiSimModel(model_name)
     elif args.jaeger:
-        model = JaegerTrace(args.jaeger[0])
+        model_name = args.jaeger[0]
+        model = JaegerTrace(model_name)
     elif args.zipkin:
-        model = ZipkinTrace(args.zipkin[0])
+        model_name = args.zipkin[0]
+        model = ZipkinTrace(model_name)
 
     if model:
         arch = Architecture(model)
@@ -60,6 +68,10 @@ if __name__ == '__main__':
                 'Successful' if success else 'Failed',
                 '' if not exceptions else '\n- ' + '\n- '.join(map(str, exceptions))))
 
+    if args.export:
+        if model_name:
+            pickle.dump(model, open(model_name + '.dat', 'wb+'))
+
     if args.d3:
 
         stop = False
@@ -78,8 +90,8 @@ if __name__ == '__main__':
 
         # Pretty
         pretty_print = False
-        if args.d3[1]:
-            pretty_print = True
+        if len(args.d3) > 1:
+            pretty_print = bool_from_string(args.d3[1])
 
         # JavaScript
         if args.d3[0].endswith('.js'):
